703a48472094359a48376407f0df7d396d8cfc6b
Aaron Kutch <aaronkutch@att.net>
2022-12-23 21:37:11 -0600

    disable block id temporarily

--- ethers-providers/src/provider.rs
+++ ethers-providers/src/provider.rs
@@ -581,11 +581,12 @@ impl<P: JsonRpcClient> Middleware for Provider<P> {
         let tx = utils::serialize(tx);
         // Some nodes (e.g. old Optimism clients) don't support a block ID being passed as a param,
         // so refrain from defaulting to BlockNumber::Latest.
-        let params = if let Some(block_id) = block {
+        /*let params = if let Some(block_id) = block {
             vec![tx, utils::serialize(&block_id)]
         } else {
             vec![tx]
-        };
+        };*/
+        let params = vec![tx];
         self.request("eth_estimateGas", params).await
     }
 
